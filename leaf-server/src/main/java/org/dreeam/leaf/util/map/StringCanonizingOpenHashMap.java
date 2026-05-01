72e7a28e8cac8730739e36af7b0d381959f0c08f
hayanesuru <hayanesuru@outlook.jp>
2026-05-01 23:27:34 +0900

    move intern

--- leaf-server/src/main/java/org/dreeam/leaf/util/map/StringCanonizingOpenHashMap.java
+++ leaf-server/src/main/java/org/dreeam/leaf/util/map/StringCanonizingOpenHashMap.java
@@ -8,9 +8,9 @@ import java.util.Map;
 
 public class StringCanonizingOpenHashMap<T> extends Object2ObjectOpenHashMap<String, T> {
 
-    private static final Interner<String> KEY_INTERNER = Interners.newBuilder().weak().concurrencyLevel(16).<String>build();
+    private static final Interner<String> KEY_INTERNER = Interners.newBuilder().weak().concurrencyLevel(16).build();
 
-    private static String intern(String key) {
+    public static String intern(String key) {
         return key != null ? KEY_INTERNER.intern(key) : null;
     }
 
@@ -27,28 +27,9 @@ public class StringCanonizingOpenHashMap<T> extends Object2ObjectOpenHashMap<Str
     }
 
     @Override
-    public T put(String key, T value) {
-        return super.put(intern(key), value);
-    }
-
-    @Override
-    public void putAll(Map<? extends String, ? extends T> m) {
-        if (m.isEmpty()) return;
-        ensureCapacity(size() + m.size());
-        for (Map.Entry<? extends String, ? extends T> entry : m.entrySet()) {
-            super.put(intern(entry.getKey()), entry.getValue());
-        }
-    }
-
-    public void putWithoutInterning(String key, T value) {
-        super.put(key, value);
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (o == this) return true;
-        if (!(o instanceof Map)) return false;
-        final Map<?, ?> m = (Map<?, ?>)o;
+        if (!(o instanceof Map<?, ?> m)) return false;
         if (m.size() != size()) return false;
         if (containsNullKey) {
             if (!value[n].equals(m.get(key[n]))) {
@@ -57,6 +38,7 @@ public class StringCanonizingOpenHashMap<T> extends Object2ObjectOpenHashMap<Str
         }
         final Object[] key = this.key;
         for (int pos = n; pos-- != 0;) {
+            //noinspection ConstantValue
             if (!((key[pos]) == null)) {
                 if (!value[pos].equals(m.get(key[pos]))) {
                     return false;
